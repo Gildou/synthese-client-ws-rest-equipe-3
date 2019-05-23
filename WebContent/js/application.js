0dea84b15fca65175e808e9f8b301483d244fbc6
IN-MP-028 <javajee2k19@gmail.com>
2019-05-23 17:52:38 +0200

    Op fichiers v1.

--- WebContent/js/application.js
+++ WebContent/js/application.js
@@ -1,12 +1,28 @@
-function chargerPersonnes() {
-	$.getJSON("http://localhost:8080/ProjetEjbWs/personnes/listPersonnes",function(data) {
+function chargerProduits() {
+	$.getJSON("http://localhost:8080/synthese-serveur-ejb-ws-equipe-3.1/magasins/allProduits",function(data) {
 		$("#tableBody").html("");
 		for(i in data) {
 			$tr=$("<tr>");
-			$td=$("<td>").append(data[i]['id']);$tr.append($td);
-			$td=$("<td>").append(data[i]['nom']);$tr.append($td);
-			$td=$("<td>").append(data[i]['prenom']);$tr.append($td);
-			$td=$("<td>").append(data[i]['age']);$tr.append($td);
+			$td=$("<td>").append(data[i]['idProduit']);$tr.append($td);
+			$td=$("<td>").append(data[i]['nomProduit']);$tr.append($td);
+			$td=$("<td>").append(data[i]['stock']);$tr.append($td);
+			$td=$("<td>").append(data[i]['prix']);$tr.append($td);
+			$td=$("<td>").append(data[i]['magasin']);$tr.append($td);
+			$("#tableBody").append($tr);
+		}
+	});
+	
+}
+
+function chargerMagasins() {
+	$.getJSON("http://localhost:8080/synthese-serveur-ejb-ws-equipe-3.1/magasins/allMagasins",function(data) {
+		$("#tableBody").html("");
+		for(i in data) {
+			$tr=$("<tr>");
+			$td=$("<td>").append(data[i]['idMagasin']);$tr.append($td);
+			$td=$("<td>").append(data[i]['nomMagasin']);$tr.append($td);
+			$td=$("<td>").append(data[i]['codeMagasin']);$tr.append($td);
+			$td=$("<td>").append(data[i]['prixDuLocal']);$tr.append($td);
 			$("#tableBody").append($tr);
 		}
 	});
